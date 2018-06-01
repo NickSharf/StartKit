b8d87203dc0e5d558d823c370a48a408a6d86b2b
Nick Sharf <nikolai.sharf@mail.ru>
2018-06-01 14:20:51 +0300

    Обновление до Gulp 4

--- gulpfile.js
+++ gulpfile.js
@@ -2,6 +2,7 @@
 
 var gulp = require('gulp');
 var sass = require('gulp-sass');
+var sassGlob = require('gulp-sass-glob');
 var plumber = require('gulp-plumber');
 var postcss = require('gulp-postcss');
 var autoprefixer = require('autoprefixer');
@@ -22,27 +23,30 @@ var ghPages = require('gulp-gh-pages');
 
 // CLEAN BUILD
 
-gulp.task('clean', function() {
-  return del('build');
+gulp.task('clean', function(done) {
+  return del('build', done);
 });
 
 // HTML
 
-gulp.task('html:del', function() {
-  return del('build/*.html');
+gulp.task('html:del', function(done) {
+  return del('build/*.html', done);
 });
 
-gulp.task('html',['html:del'], function() {
+gulp.task('html:copy', function() {
   return gulp.src('*.html')
     .pipe(rigger())
     .pipe(gulp.dest('build'));
 });
 
+gulp.task('html', gulp.series('html:del', 'html:copy'));
+
 // CSS
 
-gulp.task('style', function() {
-  gulp.src('sass/style.scss')
+gulp.task('style', function (done) {
+  gulp.src('sass/main.scss')
     .pipe(plumber())
+    .pipe(sassGlob())
     .pipe(sass({
       includePaths: require("node-normalize-scss").includePaths
     }))
@@ -52,28 +56,31 @@ gulp.task('style', function() {
         sort: true
       })
     ]))
+    .pipe(rename('style.css'))
     .pipe(gulp.dest('build/css'))
     .pipe(minifycss())
     .pipe(rename('style.min.css'))
     .pipe(gulp.dest('build/css'))
     .pipe(server.stream());
+    done()
 });
 
 // JS
-gulp.task('js:del', function() {
-  return del('build/js');
+gulp.task('js:del', function(done) {
+  return del('build/js', done);
 });
 
-gulp.task('js:libraries', ['js:del'], function() {
+gulp.task('js:libraries', function(done) {
   gulp.src('js/libraries/*.js')
     .pipe(plumber())
     .pipe(uglify())
     .pipe(gulp.dest('build/js/libraries'))
     .pipe(server.stream());
+    done()
 });
 
 
-gulp.task('js', ['js:libraries'], function() {
+gulp.task('js:scripts', function(done) {
   gulp.src('js/main.js')
     .pipe(plumber())
     .pipe(rigger())
@@ -82,42 +89,52 @@ gulp.task('js', ['js:libraries'], function() {
     .pipe(rename('main.min.js'))
     .pipe(gulp.dest('build/js'))
     .pipe(server.stream());
+    done()
 });
+
+gulp.task('js', gulp.series('js:del', 'js:libraries', 'js:scripts'));
 
 // FONTS
 
-gulp.task('fonts:del', function() {
-  return del('build/fonts');
+gulp.task('fonts:del', function(done) {
+  return del('build/fonts', done);
 });
 
-gulp.task('fonts', ['fonts:del'], function() {
+gulp.task('fonts:copy', function(done) {
   return gulp.src('fonts/**/*.{woff,woff2}')
     .pipe(gulp.dest('build/fonts/'));
+    done()
 });
 
-// FAVICON
+gulp.task('fonts', gulp.series('fonts:del', 'fonts:copy'));
 
-gulp.task('favicons:del', function() {
-  return del('build/img/favicons');
+// FAVICONS
+
+gulp.task('favicons:del', function(done) {
+  return del('build/img/favicons', done);
 });
 
-gulp.task('favicon', ['favicons:del'], function() {
+gulp.task('favicons:copy', function(done) {
   return gulp.src('img/favicons/*.{png,jpg,json,jpeg,svg}')
     .pipe(gulp.dest('build/img/favicons/'));
+    done()
 });
+
+gulp.task('favicons', gulp.series('favicons:del', 'favicons:copy'));
 
 // IMAGES
 
-gulp.task('img:del', function() {
-  return del('build/img/*.*');
+gulp.task('img:del', function(done) {
+  return del('build/img/*.*', done);
 });
 
-gulp.task('img:copy', ['img:del'], function() {
+gulp.task('img:copy', function(done) {
   return gulp.src('img/*.{png,jpg,gif,svg}')
     .pipe(gulp.dest('build/img/'));
+    done()
 });
 
-gulp.task('img:minify', ['img:copy'], function() {
+gulp.task('img:minify', function(done) {
   return gulp.src('build/img/**/*.{png,jpg,gif,svg}')
     .pipe(imagemin([
       imagemin.optipng({optimizationLevel: 3}),
@@ -125,32 +142,41 @@ gulp.task('img:minify', ['img:copy'], function() {
       imagemin.svgo()
     ]))
     .pipe(gulp.dest('build/img'));
+    done()
 });
+
+gulp.task('img', gulp.series('img:del', 'img:copy', 'img:minify'));
 
 // WEBP
 
-gulp.task('webp:del', function() {
-  return del('build/img/content');
+gulp.task('webp:del', function(done) {
+  return del('build/img/content', done);
 });
 
-gulp.task('webp:copy', ['webp:del'], function() {
+gulp.task('webp:copy', function(done) {
   return gulp.src('img/content/*.{png,jpg}')
     .pipe(gulp.dest('build/img/content'));
+    done()
 });
 
-gulp.task('webp:convert', ['webp:copy'], function() {
+gulp.task('webp:convert', function(done) {
   return gulp.src('build/img/content/*.{png,jpg}')
     .pipe(webp({quality: 90}))
     .pipe(gulp.dest('build/img/content'));
+    done()
 });
+;
+gulp.task('webp', gulp.series('webp:del', 'webp:copy', 'webp:convert'));
+
 
 // SVG-SPRITE
 
-gulp.task('svg-sprite:del', function() {
+gulp.task('svg-sprite:del', function(done) {
   return del('build/img/svg-sprite');
+  done()
 });
 
-gulp.task('svg-sprite', ['svg-sprite:del'], function() {
+gulp.task('svg-sprite:copy', function(done) {
   return gulp.src('img/svg-sprite/*.svg')
     .pipe(gulp.dest('build/img/svg-sprite'))
     .pipe(svgmin())
@@ -159,19 +185,27 @@ gulp.task('svg-sprite', ['svg-sprite:del'], function() {
     }))
     .pipe(rename('sprite.svg'))
     .pipe(gulp.dest('build/img'));
+    done()
 });
 
+gulp.task('svg-sprite', gulp.series('svg-sprite:del', 'svg-sprite:copy'));
 
 //GH-PAGES
 
-gulp.task('deploy', function() {
+gulp.task('deploy', function(done) {
   return gulp.src('build/**/*')
     .pipe(ghPages());
+    done()
 });
 
 // LIVE SERVER
 
-gulp.task('serve', function() {
+gulp.task('reload', function(done){
+  server.reload();
+  done();
+});
+
+gulp.task('serve', function(done) {
   server.init({
     server: 'build',
     notify: false,
@@ -180,24 +214,23 @@ gulp.task('serve', function() {
     ui: false
   });
 
-  gulp.watch('sass/**/*.{scss,sass}', ['style']);
-  gulp.watch('*.html', ['html', server.reload]);
-  gulp.watch('js/scripts/*.js', ['js', server.reload]);
+  gulp.watch('sass/**/*.{scss,sass}', gulp.series('style'));
+  gulp.watch('*.html', gulp.series('html', 'reload'));
+  gulp.watch('js/scripts/*.js', gulp.series('js', 'reload'));
+  done()
 });
 
 // BUILD
 
-gulp.task('build', function(done) {
-  run(
-    'clean',
+gulp.task('build', gulp.series('clean',
+  gulp.parallel(
     'html',
     'style',
     'js',
     'fonts',
-    'favicon',
-    'img:minify',
-    'webp:convert',
-    'svg-sprite',
-    done
-  );
-});
+    'favicons',
+    'img',
+    'webp',
+    'svg-sprite'
+  )
+));
--- sass/Readme.md
+++ sass/Readme.md
@@ -1,11 +1,11 @@
 Файлы для сборки стилей<br>
 
 ```bash
-blocks/          # блоки
+blocks/           # блоки
 global/            
-  fonts.scss     # шрифты проекта
-  global.scss    # глобальные стили
-mixins.scss      # миксины
-style.scss       # подключение всех стилевых файлов
-variables.scss   # переменные
+  fonts.scss      # шрифты проекта
+  global.scss     # глобальные стили
+  mixins.scss     # миксины
+  variables.scss  # переменные
+main.scss         # подключение всех стилевых файлов
 ```
